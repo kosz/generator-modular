f2d1073c03bd5729b9bb7ff9011cb95b2cad457e
Cosmin Ronnin <coszmin@gmail.com>
2014-12-25 11:20:02 -0500

    Fixing identation: spaces instead of tabs

--- generators/app/index.js
+++ generators/app/index.js
@@ -8,71 +8,71 @@ module.exports = generators.Base.extend({
 
 module.exports = generators.Base.extend({
 
-	promptProjectType: function () {
+  promptProjectType: function () {
     var done = this.async();
-		this.prompt({
-			type: 'list',
-			name: 'projectType',
-			message: 'What type of project will this be ?',
-			choices: [
-			"Angular Web App",
-			"Module to be included in other Web Apps"
-			],
-			filter: function (val) {
-				var filterMap = {
-					"Angular Web App": 'Webapp', 
-					"Module to be included in other Angular Web Apps": 'Module'
-				}
-				return filterMap[val];
-			},	
-			store: true,
-		}, function (answers) {
+    this.prompt({
+      type: 'list',
+      name: 'projectType',
+      message: 'What type of project will this be ?',
+      choices: [
+      "Angular Web App",
+      "Module to be included in other Web Apps"
+      ],
+      filter: function (val) {
+        var filterMap = {
+          "Angular Web App": 'Webapp', 
+          "Module to be included in other Angular Web Apps": 'Module'
+        }
+        return filterMap[val];
+      },  
+      store: true,
+    }, function (answers) {
       this.log(answers.projectType);
-			this.projectType = answers.projectType;
+      this.projectType = answers.projectType;
       done();
     }.bind(this));
   },
 
-	promptProjectName: function () {
+  promptProjectName: function () {
     var done = this.async();
     this.prompt({
       type    : 'input',
       name    : 'name',
       message : 'Enter a name for your ' + this.projectType + ' project.',
-			store	  : true,
+      store   : true,
       default : this.appname
     }, function (answers) {
       this.log(answers.name);
-			this.name = answers.name;
+      this.name = answers.name;
       done();
     }.bind(this));
   },
 
-	promptAngularVersion: function () {
+  promptAngularVersion: function () {
     var done = this.async();
     this.prompt({
       type: 'input',
       name: 'angularVersion',
       message: 'Which angular version would you like to use ? (eg: 1.2.28)',
-			store: true,
+      store: true,
       default: '1.3.8',
     }, function (answers) {
       this.log(answers.angularVersion);
-			this.angularVersion = answers.angularVersion;
+      this.angularVersion = answers.angularVersion;
       done();
     }.bind(this));
   },
 
-	processTemplates: function () {
+  processTemplates: function () {
 
-		this.template('gulpfile.js', 'gulpfile.js');
-		this.template('.gitignore', '.gitignore');
-		this.template('bower.json', 'bower.json');
-		this.template('package.json', 'package.json');
+    this.template('gulpfile.js', 'gulpfile.js');
+    this.template('.gitignore', '.gitignore');
+    this.template('bower.json', 'bower.json');
+    this.template('package.json', 'package.json');
 
-		this.template('src/index.html', 'src/index.html');
+    this.template('src/index.html', 'src/index.html');
 
-		this.template('src/app/app.js', 'src/app/app.js');
+    this.template('src/app/app.js', 'src/app/app.js');
 
   }
 

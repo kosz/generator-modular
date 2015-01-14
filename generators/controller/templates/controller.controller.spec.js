da865c78b82bc49d3da9cc5e81186f0f4253b6a0
Cosmin Ronnin <coszmin@gmail.com>
2015-01-13 22:04:01 -0500

    chore(*): replaced tabs with spaces

--- generators/controller/templates/controller.controller.spec.js
+++ generators/controller/templates/controller.controller.spec.js
@@ -1,34 +1,34 @@
 describe('Controller: <%= controllerName %>', function() {
   var controller, $rootScope;
-	<% if (injections.length > 0) {  %>var <%= injections.join(',') %>;<% } %>
+    <% if (injections.length > 0) {  %>var <%= injections.join(',') %>;<% } %>
 
   beforeEach(module('<%= config.get('name') %>')); <%
 
 %>
-	beforeEach(inject(function (<%
-		var mockInjections = ['$controller','$rootScope'].concat(injections);
-		mockInjections = generatorWebappUtils.wrapAngularMockInjections(mockInjections);
-		var scopeIdx = mockInjections.indexOf('_$scope_');
-		var hasScope = false;
-		if (  scopeIdx > -1 ) {
-			mockInjections.splice(scopeIdx,1);
-			hasScope = true;
-		}
-		 %><%= mockInjections.join(',') %>) { 
+    beforeEach(inject(function (<%
+        var mockInjections = ['$controller','$rootScope'].concat(injections);
+        mockInjections = generatorWebappUtils.wrapAngularMockInjections(mockInjections);
+        var scopeIdx = mockInjections.indexOf('_$scope_');
+        var hasScope = false;
+        if (  scopeIdx > -1 ) {
+            mockInjections.splice(scopeIdx,1);
+            hasScope = true;
+        }
+         %><%= mockInjections.join(',') %>) { 
     <% 
-		mockInjections.forEach(function(injection) {
-		if ( injection !== '_$scope_' ) {
+        mockInjections.forEach(function(injection) {
+        if ( injection !== '_$scope_' ) {
 %>
-		<%= injection.replace(/_/g, '') + ' = ' + injection %>;<% } else { 
-		%> 
-		$scope = $rootScope.$new(); <%
+        <%= injection.replace(/_/g, '') + ' = ' + injection %>;<% } else { 
+        %> 
+        $scope = $rootScope.$new(); <%
 }}) %>
-		<%= hasScope ? '$scope = $rootScope.$new();' : '' %>
+        <%= hasScope ? '$scope = $rootScope.$new();' : '' %>
 
     controller = $controller('<%= controllerName %>', { <% 
-			injections.forEach(function(injection,index) { %>
-		    '<%= injection %>': <%= injection %><%= injections.length > index+1 ? ',' : '' %><%
-		});
+            injections.forEach(function(injection,index) { %>
+            '<%= injection %>': <%= injection %><%= injections.length > index+1 ? ',' : '' %><%
+        });
 %>
       });
   }));
@@ -40,9 +40,9 @@ describe('Controller: <%= controllerName %>', function() {
     scopeMethods.forEach(function(method,index) {
   %>
 
-	it('should define <%= scope === "controller" ? "" : scope %>.<%= method %>', function () {
-		expect(angular.isFunction(<%= scope %>.<%= method %>)).toBe(true);
-		// TODO: add tests for other behaviour here
+  it('should define <%= scope === "controller" ? "" : scope %>.<%= method %>', function () {
+    expect(angular.isFunction(<%= scope %>.<%= method %>)).toBe(true);
+    // TODO: add tests for other behaviour here
   });<% }); %>
 
 });
